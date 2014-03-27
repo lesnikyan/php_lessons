63529b802701e38309773e826afc7c00b0ab6624
sergio lesnik <sergiolesnik@gmail.com>
2014-03-27 17:21:10 +0200

    functions

---
+++ www/lesson2/functions.php
@@ -0,0 +1,18 @@
+<?php
+$foo = function(){ p ('foo func'); };
+$foo();
+
+$lambda = create_function('$a, $b, $c', 'return " $a * $b * $c = " . $a * $b * $c;');
+p($lambda (3,4,5));
+
+p('call_user_func_array = ' . call_user_func_array($lambda, array(2,3,4)));
+
+//function lnktest($x){ $x[] = 5; return $x;}
+function lnktest(&$x){ $x[] = 5; return $x;}
+p(imp(array(1,2,3,4,5,6,7,8,9)));
+$lnk = array(8);
+$lnk2 = lnktest($lnk);
+p("lnk = ". imp($lnk) . " lnk2 = " . imp($lnk2));
+$lnkname = 'lnktest';
+$lnkNameVal = array(2,3);
+p("lnkName = " . imp($lnkname($lnkNameVal)));
