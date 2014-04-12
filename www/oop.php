8a45353cb9f10854f5b85f4a0b608df09cdf8f0f
sergio lesnik <sergiolesnik@gmail.com>
2014-04-12 13:10:50 +0300

    oop, comments

--- www/oop.php
+++ www/oop.php
@@ -1,25 +1,25 @@
 <?php
-
+// примесь
 trait MoveAction{
 	protected function inAction(){ return "На прогулке "; }
 }
-
+//интерфейс
 interface Movable{
 	function walk();
 }
-
+//базовый абстрактный класс
 abstract class Animal implements Movable{
 	use MoveAction;
 //	abstract function walk();
 }
-
+//реализация 1
 class Dog extends Animal{
 	function walk(){
 		print $this->inAction() . $this->run() . "<br />";
 	}
 	function run(){return "цобак бегает";}
 }
-
+// реализация 2
 class Fish extends Animal{
 	function walk(){
 		print $this->inAction() . $this->swim() . "<br />";
@@ -28,11 +28,11 @@ class Fish extends Animal{
 		return "рыба плавает";
 	}
 }
-
-function walkZoo(Animal $animal){
+// using Movable interface
+function walkZoo(Movable $animal){
 	$animal->walk();
 }
-
+// testing
 $animals = [new Dog, new Fish];
 
 foreach($animals as $one){ walkZoo($one); }
